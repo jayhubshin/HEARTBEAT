ba354bcd64124875d82e36782b678d8d1b766b19
jayhubshin <75716637+jayhubshin@users.noreply.github.com>
2026-03-25 15:47:08 +0900

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js
-const CONFIG = {
-    SUPABASE_URL: "https://gkwtucqymzkvpurcpihk.supabase.co",
-    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdrd3R1Y3F5bXprdnB1cmNwaWhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM5MDIxNDcsImV4cCI6MjA4OTQ3ODE0N30.V0FnaZ-BaTEYUOKfzxvQ-T4Qk4E83LNIi4GflQsURUg"
-};
